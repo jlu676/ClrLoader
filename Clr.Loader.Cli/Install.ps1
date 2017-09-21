6854663e9df9fb7febf7307496635f5fe2a2875c
Luther <luther.j@gmail.com>
2017-09-21 16:05:49 -0500

    added user input to install and remove xml files

---
+++ Clr.Loader.Cli/Install.ps1
@@ -0,0 +1,12 @@
+﻿$installPath = Read-Host -Prompt "Input install path"
+
+if(!(test-path $installPath))
+{
+	Write-host "Invalid path or path does not exist $installPath"
+}
+else 
+{
+	$msbuild = 'C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe'
+	& $msbuild ../Clr.Loader.sln /p:Configuration=Release /p:OutDir=$installPath /t:Build 
+	Write-host "Installed"
+}
